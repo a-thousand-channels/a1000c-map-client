0504b3f464de4ab1c4cc38b6c520d010abcd2bd1
Ulf Treger <ulf.treger@3plusx.de>
2021-10-20 17:31:16 +0200

    Basic splash page content

---
+++ components/Splash.vue
@@ -0,0 +1,30 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-pink-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-12">
+      <div class="flex justify-center pt-8 sm:pt-0">
+        <img src="a-thousand-channels--logo-variant-a--short.png" title="A thousand channels">
+      </div>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Welcome to the early prototype of our web map.
+        </h2>
+        <p class="mt-3 text-gray-600">
+          We are developing a static web map, that can easily be generated and published on the web. You will not need server side technologies for that, just a simple webspace. This prototype is work in progress.
+        </p>
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+          Please feel free to try out. We would very happy about feedback and contributions.
+        </p>
+        <p class="mt-4 pt-4 text-gray-800">
+          Give us a shout if you have questions or need help. Please visit the <a href="https://github.com/a-thousand-channels/a1000c-map-client" style="text-decoration: none; background-image: linear-gradient(120deg, #fde68a 0, #fde68a 100%); background-repeat: no-repeat; background-size: 100% 0.4em; background-position: 0 100%">Github repository for this project</a>.
+        </p>
+      </div>
+      <div class="flex justify-center pt-4 space-x-2">
+        <a href="https://www.a-thousand-channels.xyz/" target="_blank" style="text-decoration: none; background-image: linear-gradient(120deg, #fde68a 0, #fde68a 100%); background-repeat: no-repeat; background-size: 100% 0.4em; background-position: 0 100%">
+          A Thousand Channels – a queer 🏳️‍🌈 mapping 🗺 platform 🎁
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <Splash />
 </template>
 
 <script>
