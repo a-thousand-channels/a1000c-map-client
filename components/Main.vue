e5c6a48a629234b56e56b09bddee2ce114be3412
Ulf Treger <ulf.treger@googlemail.com>
2021-10-26 15:29:12 +0200

    Navigation by key strokes (arrowleft, arrowright)

--- components/Main.vue
+++ components/Main.vue
@@ -54,23 +54,62 @@
   <div class="flex a1000c-horizontal">
     <section id="info" class="flex min-h-screen bg-a100c-1 sm:items-center sm:pt-0">
         <div class="flex content items-center justify-center">
-          <h2>Info</h2>
+          <h2 class="bg-white-500 px-2 py-1 rounded">Info</h2>
         </div>
         <a href="#map" class="nav flex block items-center justify-center text-white font-bold">&gt;</a>
     </section>
     <section id="map" class="flex min-h-screen bg-a100c-2 sm:items-center sm:pt-0">
       <a href="#info" class="nav flex items-center justify-center text-white font-bold">&lt;</a>
       <div class="flex content items-center justify-center">
-        <h2>Map</h2>
+        <h2 class="bg-white-500 px-2 py-1 rounded">Map</h2>
+        <p>
+          <button class="hidden" v-shortkey="['arrowleft']" @shortkey="navigate_left()">&lt;</button>
+          <button class="hidden" v-shortkey="['arrowright']" @shortkey="navigate_right()">&gt;</button>
+        </p>
+
       </div>
       <a href="#list" class="nav flex block items-center justify-center text-white font-bold">&gt;</a>
     </section>
     <section id="list" class="flex min-h-screen bg-a100c-3 sm:items-center sm:pt-0">
       <a href="#map" class="nav flex items-center justify-center text-white font-bold">&lt;</a>
       <div class=" content flex items-center justify-center">
-        <h2>List</h2>
+        <h2 class="bg-white-500 px-2 py-1 rounded">List</h2>
       </div>
     </section>
   </div>
 </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  mounted: function() {
+    // this.jumpToMap()
+    this.$router.push({ name: 'main', hash: '#map' })
+  },
+  methods: {
+    jumpToMap() {
+      console.log("jumpToMap")
+      this.$router.push({ name: 'main', hash: '#map' })
+    },
+    navigate_left() {
+      console.log( "<-- from "+this.$route.hash)
+      if ( this.$route.hash == '#map' ) {
+        this.$router.push({ name: 'main', hash: '#info' })
+      }
+      else if ( this.$route.hash == '#list' ) {
+        this.$router.push({ name: 'main', hash: '#map' })
+      }
+    },
+    navigate_right() {
+      console.log( "--> from " +this.$route.hash)
+      if ( this.$route.hash == '#info' ) {
+        this.$router.push({ name: 'main', hash: '#map' })
+      }
+      else if ( this.$route.hash == '#map' ) {
+        this.$router.push({ name: 'main', hash: '#list' })
+      }
+    }
+  }
+};
+</script>
