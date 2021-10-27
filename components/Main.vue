244241990e9bd02e4693609e00ab22e8f62f114d
Ulf Treger <ulf.treger@googlemail.com>
2021-10-27 11:21:30 +0200

    Improve/fix keystroke navigation, add snap scroll behaviour

--- components/Main.vue
+++ components/Main.vue
@@ -1,49 +1,65 @@
 <style>
     body {
       scroll-behavior: smooth;
-      overflow: hidden;
+      overflow-y: hidden;
+      overflow-x: auto;
+
     }
     #page {
+
     }
     .a1000c-horizontal {
+      height: 100%;
+      display: flex;
+      /* important */
+      overflow-y: hidden;
+      overflow-x: auto;
+      scroll-snap-type: x mandatory;
     }
     .a1000c-horizontal > .flex {
       flex-shrink: 0;
-     }
-     .nav {
+      width: 100vw;
+
+      /* important */
+      scroll-snap-align: center;
+
+    }
+    .nav {
       flex-shrink: 1;
       flex-basis: 4%;
-      background-color: rgba(20,20,20,0.1);
-     }
-     .nav:hover {
-      background-color: rgba(20,20,20,0.35);
-
-     }
-     .content {
-      flex-shrink: 1;
-      flex-basis: 97%;
-     }
-     #map > .content {
-      flex-shrink: 1;
-      flex-basis: 87%;
-     }
+      width: 20px;
+      background-color: rgba(20,20,20,0);
+      color: rgba(255,255,255,0.1);
+    }
+    .nav:hover {
+      background-color: rgba(20,20,20,0);
+      color: rgba(255,255,255,1);
+    }
+    .content {
+    flex-shrink: 1;
+    flex-basis: 97%;
+    }
+    #map > .content {
+    flex-shrink: 1;
+    flex-basis: 93%;
+    }
    .bg-a100c-1 {
       background: rgb(255,0,249);
       background: linear-gradient(90deg, rgba(255,0,249,0.15) 0%, rgba(255,117,0,0.15) 0%, rgba(255,0,35,0.15) 100%);
-      width: 100vw;
    }
    .bg-a100c-2 {
       background: rgb(255,0,249);
       background: linear-gradient(90deg, rgba(255,117,0,0.15) 0%, rgba(255,0,35,0.15) 50%, rgba(255,0,249,0.15) 100% );
-      width: 100vw;
    }
    .bg-a100c-3 {
       background: rgb(255,0,249);
       background: linear-gradient(90deg, rgba(255,0,35,0.15) 0%, rgba(255,0,249,0.15) 50%, rgba(255,117,0,0.15) 100% );
-      width: 100vw;
+   }
+   .bg-a100c-white {
+      background-color: rgba(255,255,255,0.8);
    }
    .text-white {
-      color: rgba(255,255,255,0.85);
+      color: rgba(255,255,255,0.4);
    }
 
 </style>
@@ -53,14 +69,14 @@
   <div class="flex a1000c-horizontal">
     <section id="info" class="flex min-h-screen bg-a100c-1 sm:items-center sm:pt-0">
         <div class="flex content items-center justify-center">
-          <h2 class="bg-white-500 px-2 py-1 rounded">Info</h2>
+          <h2 class="bg-a100c-white px-2 py-1 rounded">Info</h2>
         </div>
         <a href="#map" class="nav flex block items-center justify-center text-white font-bold">&gt;</a>
     </section>
     <section id="map" class="flex min-h-screen bg-a100c-2 sm:items-center sm:pt-0">
       <a href="#info" class="nav flex items-center justify-center text-white font-bold">&lt;</a>
       <div class="flex content items-center justify-center">
-        <h2 class="bg-white-500 px-2 py-1 rounded">Map</h2>
+        <h2 class="bg-a100c-white px-2 py-1 rounded">Map</h2>
         <p>
           <button class="hidden" v-shortkey="['arrowleft']" @shortkey="navigate_left()">&lt;</button>
           <button class="hidden" v-shortkey="['arrowright']" @shortkey="navigate_right()">&gt;</button>
@@ -72,7 +88,7 @@
     <section id="list" class="flex min-h-screen bg-a100c-3 sm:items-center sm:pt-0">
       <a href="#map" class="nav flex items-center justify-center text-white font-bold">&lt;</a>
       <div class=" content flex items-center justify-center">
-        <h2 class="bg-white-500 px-2 py-1 rounded">List</h2>
+        <h2 class="bg-a100c-white px-2 py-1 rounded">List</h2>
       </div>
     </section>
   </div>
@@ -93,20 +109,28 @@ export default {
     },
     navigate_left() {
       console.log( "<-- from "+this.$route.hash)
+      let to = '';
       if ( this.$route.hash == '#map' ) {
-        this.$router.push({ name: 'main', hash: '#info' })
+        to = '#info'
+      } else if ( this.$route.hash == '#list' ) {
+        to = '#map'
       }
-      else if ( this.$route.hash == '#list' ) {
-        this.$router.push({ name: 'main', hash: '#map' })
+      if (to) {
+        this.$router.push({ name: 'main', hash: to })
+        location.hash = to;
       }
     },
     navigate_right() {
       console.log( "--> from " +this.$route.hash)
-      if ( this.$route.hash == '#info' ) {
-        this.$router.push({ name: 'main', hash: '#map' })
+      let to = '';
+      if ( this.$route.hash == '#map' ) {
+        to = '#list'
+      } else if ( this.$route.hash == '#info' ) {
+        to = '#map'
       }
-      else if ( this.$route.hash == '#map' ) {
-        this.$router.push({ name: 'main', hash: '#list' })
+      if (to) {
+        this.$router.push({ name: 'main', hash: to })
+        location.hash = to;
       }
     }
   }
