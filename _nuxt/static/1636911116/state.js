8ceecea10439cfaf8457b003e7e14520cc98967a
Jarami110d <Jarami110d@users.noreply.github.com>
2021-11-14 17:31:58 +0000

    deploy: b90107745ea34aebabecbab68072883edddecaec

---
+++ _nuxt/static/1636911116/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fa1000c-map-client\u002F_nuxt\u002Fstatic\u002F1636911116",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fa1000c-map-client\u002F",assetsPath:"\u002Fa1000c-map-client\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"26e17174"}}}}(null));
