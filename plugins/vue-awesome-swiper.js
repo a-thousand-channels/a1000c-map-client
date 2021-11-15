f6d28bdaaf19fcee5d9c0aa161d8086440a5329e
Ulf Treger <ulf.treger@googlemail.com>
2021-11-15 19:04:13 +0100

    Images: Add swiper js

---
+++ plugins/vue-awesome-swiper.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// import style
+import 'swiper/swiper.min.css';
+
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
