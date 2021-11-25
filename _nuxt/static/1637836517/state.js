2fbb614d6c62c0a68580676ab3e9799aaa097d29
ut <ut@users.noreply.github.com>
2021-11-25 10:35:19 +0000

    deploy: 9869b12f8a23806d12dabc540df549fe7d2ab2c5

---
+++ _nuxt/static/1637836517/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fa1000c-map-client\u002F_nuxt\u002Fstatic\u002F1637836517",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fa1000c-map-client\u002F",assetsPath:"\u002Fa1000c-map-client\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e2df7845"}}}}(null));
