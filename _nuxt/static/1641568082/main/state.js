44cf3c08a7b84b691d566ce8c5c5f7aada5cf4db
ut <ut@users.noreply.github.com>
2022-01-07 16:32:38 +0000

    deploy: db4554468d13797728163ce27045dcdf564dbe08

--- _nuxt/static/1641568082/main/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fa1000c-map-client\u002F_nuxt\u002Fstatic\u002F1641568082",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmain",config:{_app:{basePath:"\u002Fa1000c-map-client\u002F",assetsPath:"\u002Fa1000c-map-client\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5dd269d9"}}}}(null));
