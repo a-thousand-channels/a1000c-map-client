936c365fd815a7d66407a9980bf65713192546ab
Sefux <Sefux@users.noreply.github.com>
2022-01-31 02:30:17 +0000

    deploy: 2e6233a39258337d92b783afbef787a6c6b310ac

--- _nuxt/static/1643595454/main/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fa1000c-map-client\u002F_nuxt\u002Fstatic\u002F1643595454",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmain",config:{_app:{basePath:"\u002Fa1000c-map-client\u002F",assetsPath:"\u002Fa1000c-map-client\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}}}}(null));
