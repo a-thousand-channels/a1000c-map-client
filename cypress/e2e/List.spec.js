9368a5e9e26fb2a8d7ffd003adccb085aa8be7ad
Ulf Treger <ulf.treger@googlemail.com>
2023-04-04 18:43:33 +0200

    E2e testing for list view and is in viewport

---
+++ cypress/e2e/List.spec.js
@@ -0,0 +1,26 @@
+describe('List', () => {
+
+  before(() => {
+    cy.visit('http://localhost:3000/');
+    cy.wait(1000);
+    cy.visit('http://localhost:3000/main');
+    cy.wait(500);
+    cy.visit('http://localhost:3000/main#list');
+  })
+
+  it('should show list content (and not the map)', () => {
+
+    cy.get('#list').should('be.visible');
+    cy.get('#list div div ul').should('not.be.empty');
+    cy.get('#list div div ul li').should('have.length.greaterThan', 0);
+
+  });
+  it('should focus on list section only', () => {
+    cy.get('#list').should(($el) => {
+      const bottom = Cypress.$(cy.state('window')).height();
+      const rect = $el[0].getBoundingClientRect();
+      expect(rect.top).to.be.at.least(0);
+      expect(rect.bottom).to.be.lessThan(bottom+10);
+    });
+  })
+});
